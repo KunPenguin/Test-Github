f4db561f7c2cfadf58a05e5f8cee9558bdaf21f2
KunPenguin <1737020588@qq.com>
2026-09-24 13:32:22 +0800

    Update Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("TEsst");
     }
 
     // Update is called once per frame
